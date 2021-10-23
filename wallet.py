be0040dcad954d0d84799e4a4d540970251356f6
Calslock <admin@calslock.net>
2021-10-23 14:03:33 +0200

    initial2

--- wallet.py
+++ wallet.py
@@ -1,22 +1,24 @@
 import tkinter as tk
-import tkinter.font as tkFont
+import hashlib as hl
+import hmac
+from Crypto.Cipher import AES
 
-loginpage = tk.Tk()
-loginpage.title("Portfel hasel v0.1")
+pepper = "70f2a64df3aa17f5833b85c9508b23266b783b15b6562c80e4c5e92dd176b1e0"
 
-logininfolabel = tk.Label(loginpage, text="Logowanie")
-logininfolabel.pack()
+loginPage = tk.Tk()
+loginPage.title("Portfel haseł v0.1")
+loginInfoLabel = tk.Label(loginPage, text="Logowanie").pack()
+loginLoginLabel = tk.Label(loginPage, text="Login").pack()
+loginLoginInput = tk.Entry(loginPage).pack()
+loginPassLabel = tk.Label(loginPage, text="Hasło").pack()
+loginPassInput = tk.Entry(loginPage, show='\u2022').pack()
 
-loginloginlabel = tk.Label(loginpage, text="Login")
-loginloginlabel.pack()
+hashChoice = tk.IntVar()
+hashChoice.set(1)
+hashLabel = tk.Label(loginPage, text="Sposób hashowania").pack()
+hashSHAradio = tk.Radiobutton(loginPage, text="AES", variable=hashChoice, value=1).pack()
+hashHMACradio = tk.Radiobutton(loginPage, text="HMAC", variable=hashChoice, value=2).pack()
 
-loginlogin = tk.Entry(loginpage)
-loginlogin.pack()
 
-loginpasslabel = tk.Label(loginpage, text="Haslo")
-loginpasslabel.pack()
-
-loginpass = tk.Entry(loginpage, show='\u2022')
-loginpass.pack()
 
 tk.mainloop()
