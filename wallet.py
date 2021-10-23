bd6ca03cdc1f16a65b40c18f3e36a2d75f9d31c9
Calslock <admin@calslock.net>
2021-10-23 16:19:21 +0200

    Added AES encryption

--- wallet.py
+++ wallet.py
@@ -7,7 +7,9 @@ import mysql.connector as conn
 import random
 import string
 
-pepper = hl.sha256(b"VewwySecretKey").hexdigest()
+pepper = hl.sha256(b"VewwySecretKey")
+binpepper = pepper.digest()
+pepper= pepper.hexdigest()
 dbhost = "localhost"
 dbuser = "root"
 dbdatabase = "fspw"
@@ -15,7 +17,7 @@ db = None
 
 loginPage = tk.Tk()
 loginPage.geometry("320x270")
-loginPage.title("Portfel haseł v0.2")
+loginPage.title("Portfel haseł v0.3")
 
 loginInfoLabel = tk.Label(loginPage, text="Logowanie").pack()
 loginLoginLabel = tk.Label(loginPage, text="Login").pack()
@@ -31,36 +33,42 @@ hashLabel = tk.Label(loginPage, text="Sposób hashowania").pack()
 hashSHAradio = tk.Radiobutton(loginPage, text="AES", variable=hashChoice, value=1).pack()
 hashHMACradio = tk.Radiobutton(loginPage, text="HMAC", variable=hashChoice, value=2).pack()
 
-def createSalt(length):
-    return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length))
 
-def tryRegister():
+def createsalt(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+
+def tryregister():
     username = loginLoginInput.get()
     password = loginPassInput.get()
-    salt = createSalt(16)
+    salt = createsalt(16)
     password = password + salt
     if hashChoice.get() == 1:
         storage = '1'
         password = hl.sha512(password.encode('UTF-8')).hexdigest()
+        cipher = AES.new(binpepper, AES.MODE_EAX)
+        password = cipher.encrypt(password.encode('UTF-8')).hex()
+        print(password)
     else:
         storage = '2'
         password = hmac.new(pepper.encode('UTF-8'), password.encode('UTF-8'), hl.sha512).hexdigest()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `users` WHERE `login` = '"+username+"'")
     res = cursor.fetchall()
-    print(cursor.fetchall())
     if res:
-        tk.Label(loginPage, text="Taki użytkownik już istnieje!").pack()
+        tk.Label(loginPage, text="Taki użytkownik już istnieje!", fg="red").pack()
     else:
-        cursor.execute("INSERT INTO `users` (`id`, `login`, `password_hash`, `salt`, `storage`) VALUES (NULL, '"+username+"', '"+password+"', '"+salt+"', '"+storage+"');")
-        tk.Label(loginPage, text="Zarejestrowano! Możesz się teraz zalogować.").pack()
+        cursor.execute("INSERT INTO `users` (`id`, `login`, `password_hash`, `salt`, `storage`) VALUES (NULL, '" +
+                       username + "', '" + password + "', '" + salt + "', '" + storage + "');")
+        tk.Label(loginPage, text="Zarejestrowano! Możesz się teraz zalogować.", fg="green").pack()
+
 
 try:
     db = conn.connect(host=dbhost, user=dbuser, database=dbdatabase)
     db.autocommit = True
     connectLabel = tk.Label(loginPage, text="Połączono z bazą danych", fg="green").pack()
     loginButton = tk.Button(loginPage, text="Zaloguj").pack()
-    loginRegisterButton = tk.Button(loginPage, text="Zarejestruj", command=tryRegister).pack()
+    loginRegisterButton = tk.Button(loginPage, text="Zarejestruj", command=tryregister).pack()
 except (_mysql_connector.MySQLInterfaceError, conn.errors.DatabaseError):
     connectLabel = tk.Label(loginPage, text="Nie udało się połączyć z bazą danych", fg="red").pack()
 
